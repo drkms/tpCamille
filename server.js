4600bdfa341303f596164d430b670abcfe3339fd
Patrick BIEDRON <patrick.biedron@yunit.eu>
2021-11-26 15:19:27 +0100

    ça marche ! node c la vie

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 //var route = require('/html/index.html');
 
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json({extended : true});
+//app.use(jsonParser);
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/html', express.static(__dirname + "/html"));
 app.get('/', function (req, res) {
     res.redirect('/html/index.html');
@@ -11,13 +16,23 @@ app.get('/', function (req, res) {
 
 
 
-app.post('/add', function (req, res) {
-    var a = req.body.aff;
-    console.log(JSON.stringify(a));
+app.post('/add', function (request, response) {
+    var p1 = request.body.p1;
+   // console.log(req);
+    console.log(p1);
+    
    // res.send(a);
-    res.redirect('/html/index.html');
+    response.send(p1);
+  
+    //res.redirect('/html/index.html');
 });
 
 app.listen(8282, function () {
     console.log("http://localhost:8282");
 });
+
+
+// app.post('/post.html', function(request, response) {
+//   var p1 = request.body.p1; 
+//   console.log("p1=" + p1);
+// });
